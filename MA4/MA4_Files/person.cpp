7c442dcddee586f7d7193663e50d3bb96aeaaca7
Ifeoluwaojo1 <ifeoluwa.ojo.9910@student.uu.se>
2023-10-18 16:17:32 +0200

    included set and get

--- MA4/MA4_Files/person.cpp
+++ MA4/MA4_Files/person.cpp
@@ -3,15 +3,24 @@
 // Reviewer: 
 // Student: Ifeoluwa Ojo
 
+
 class Person{
 	public:
 		Person(int);
 		int fib();
+		int get();
+		void set(int);
 	private:
+
 		int fibonacciRecursive(int);
 		int age;
 	};
-
+int Person::get(){
+		return age;
+		}
+void Person::set(int n) {
+	 	age = n;
+}
 int Person::fibonacciRecursive(int n){
 	if (n <= 1) {
 		return n;
@@ -30,6 +39,8 @@ age = n;
 
 extern "C"{
 	Person* Person_new(int n) {return new Person(n);}
+	int Person_get(Person* person) {return person->get();}
+	void Person_set(Person* person, int n) {return person->set(n);}
 	int Person_fib(Person* person) {return person->fib();}
 	void Person_delete(Person* person){
 		if (person){
